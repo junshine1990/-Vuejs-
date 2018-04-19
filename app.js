dbe4b9e7f146ea08201431661351c939fbba46d6
junshine1990 <791100345@qq.com>
2018-04-19 15:15:10 +0800

    数据和方法简单介绍
    数据和方法简单介绍

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ let app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Junshine',
+        job: 'Web开发!'
+    },
+
+    methods: {
+        // this.data.job = this.job
+        greet(msg){
+            return `Good Junshine ${msg} 喜欢 ${this.job}`;
+        }
+        
     }
 
 })
 
 //el element需要获取的元素，一定是html中的根容器 表示vue需要一个根容器
+
+//data 用于数据的存储，
+//在方法中访问data中的数据 直接（this是当前的vue实例）通过 this.属性 即可
+//在方法中访问method中的方法 直接（this是当前的vue实例）通过 this.方法名 即可
