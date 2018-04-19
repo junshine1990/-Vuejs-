b1440d3d7139fc26524de48de3ccb4dcb1dbf5af
junshine1990 <791100345@qq.com>
2018-04-19 15:28:21 +0800

    属性绑定
    属性绑定

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 let app = new Vue({
     el: '#app',
     data: {
+
         message: 'Hello Junshine',
-        job: 'Web开发!'
+        job: 'Web开发!',
+        website: 'https://junshine1990.github.io',
+        websiteTag: '<a href="https://junshine1990.github.io">junshine</a>',
     },
 
     methods: {
@@ -20,5 +23,11 @@ let app = new Vue({
 //el element需要获取的元素，一定是html中的根容器 表示vue需要一个根容器
 
 //data 用于数据的存储，
+// methods 用于存储各种方法。
+
 //在方法中访问data中的数据 直接（this是当前的vue实例）通过 this.属性 即可
 //在方法中访问method中的方法 直接（this是当前的vue实例）通过 this.方法名 即可
+
+//属性绑定 v-bind 将data中的数据绑定到标签中的某个属性中
+//  <a v-bind:href="website">junshine1990.github.io</a>
+//  <a :href="website">junshine1990.github.io</a>
