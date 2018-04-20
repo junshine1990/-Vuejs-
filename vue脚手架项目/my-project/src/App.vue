2b2b816a2784b1d1dedc53542b493284d6216600
junshine1990 <791100345@qq.com>
2018-04-20 21:46:40 +0800

    脚手架创建项目以及局部组件的注册
    脚手架创建项目以及局部组件的注册

---
+++ vue脚手架项目/my-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  name: 'App',
+  components: { Home },
+  data(){
+    return {
+      msg: '这是我的Vue脚手架项目',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+  text-align: center;
+  
+}
+</style>
---
+++ vue脚手架项目/my-project/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">   
+        <h1>Home</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'home',
+        data(){
+            return {
+                users: ['jjx','hjl','zsf'],
+
+            }
+        }
+
+    }
+
+
+</script>
+
+<style>
+
+
+</style>
