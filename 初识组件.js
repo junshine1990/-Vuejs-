6096fc7159efd797ae981d51f514157947ec79f0
junshine1990 <791100345@qq.com>
2018-04-20 18:42:28 +0800

    初识全局注册组件
    初识全局注册组件，组件中的data必须是一个函数，而且函数的返回值是一个对象。如果是一个值类型的化，一处改处处改。

---
+++ 初识组件.js
@@ -0,0 +1,19 @@
+
+
+Vue.component('greeting',{
+    template: '<p>大家好，给大家介绍一下Vue的组件@赵奕欢</p>'
+})
+//实例化Vue对象
+let appOne = new Vue({
+    el: '#app-one',
+  
+
+})
+ 
+
+
+let appTwo = new Vue({
+    el: '#app-two',
+  
+
+})
